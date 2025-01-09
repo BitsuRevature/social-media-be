69eebc73d8a150d5c1062538fc463325039a25e0
Bitsu <bistua.mamo@gmail.com>
2025-01-09 14:14:46 -0600

    Login and Register Validation

---
+++ src/main/java/com/bitsu/social_media/exception/ValidationException.java
@@ -0,0 +1,16 @@
+package com.bitsu.social_media.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class ValidationException {
+
+}
