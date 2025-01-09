a0261488b42b20efe76a7d42de735e43021d15e6
Bitsu <bistua.mamo@gmail.com>
2025-01-09 14:49:02 -0600

    Add: Comment Validation

--- src/main/java/com/bitsu/social_media/dto/CommentRequest.java
+++ src/main/java/com/bitsu/social_media/dto/CommentRequest.java
@@ -1,5 +1,8 @@
 package com.bitsu.social_media.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentRequest {
+
+    @NotBlank
+    @Size(min = 1, max = 255, message = "Content must be between 1 and 255 characters")
     private String content;
+
+    @NotNull
     private int postId;
 }
